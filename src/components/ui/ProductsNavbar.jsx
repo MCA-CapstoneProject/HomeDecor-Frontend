1bfc4a9730d4c750065d8beb9a1f187e8f99ce47
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-06-30 00:56:32 +0530

    added products navbar

--- src/components/ui/ProductsNavbar.jsx
+++ src/components/ui/ProductsNavbar.jsx
@@ -10,32 +10,42 @@ const ProductsNavbar = () => {
         {/* <!-- Main navigation container --> */}
         <nav
           class="flex justify-center items-center  py-2 shadow-lg bg-[#755c5c] text-white">
-          <div class="flex w-full flex-wrap items-center  px-3"> 
+          <div class="flex w-full flex-wrap items-center  px-3">
             {/* <!-- Collapsible navbar container --> */}
             <div class="mt-2 flex-grow basis-[100%] items-center">
               <ul class="list-style-none flex justify-center space-x-10">
                 {/* <!-- Home link --> */}
-                <li class="my-4 lg:my-0 lg:pr-2 hover:border-b-[4px]" data-te-nav-item-ref>
-                  <a class="active" aria-current="page" href="#">
-                  Plant decoration 
-                  </a>
-                </li>
+                <Link to="">
+                  <li class="my-4 lg:my-0 lg:pr-2 hover:border-b-[4px]" data-te-nav-item-ref>
+                    <a class="active" aria-current="page">
+                      Plant decoration
+                    </a>
+                  </li>
+                </Link>
                 {/* <!-- Features link --> */}
-                <li class="  hover:border-b-[4px]" data-te-nav-item-ref>
-                  <a class="p-0 hover:text-neutral-70" href="#">
-                    Art and Frames</a>
-                </li>
+                <Link to="">
+                  <li class="  hover:border-b-[4px]" data-te-nav-item-ref>
+                    <a class="p-0 hover:text-neutral-70">
+                      Art and Frames</a>
+                  </li>
+                </Link>
                 {/* <!-- Pricing link --> */}
-                <li class="  hover:border-b-[4px]" data-te-nav-item-ref>
-                  <a class="p-0 hover:text-neutral-70" href="#">
-                    Interior Lightings</a>
-                </li>
-                <li class="hover:border-b-[4px]" data-te-nav-item-ref>
-                  <a class="lg:px-2"aria-current="page" href="#">Mirrors</a>
-                </li>
-                <li class="hover:border-b-[4px]" data-te-nav-item-ref>
-                  <a class="lg:px-2" aria-current="page" href="#">ShowPiece</a>
-                </li>
+                <Link to="">
+                  <li class="  hover:border-b-[4px]" data-te-nav-item-ref>
+                    <a class="p-0 hover:text-neutral-70">
+                      Interior Lightings</a>
+                  </li>
+                </Link>
+                <Link to="">
+                  <li class="hover:border-b-[4px]" data-te-nav-item-ref>
+                    <a class="lg:px-2" aria-current="page">Mirrors</a>
+                  </li>
+                </Link>
+                <Link to="">
+                  <li class="hover:border-b-[4px]" data-te-nav-item-ref>
+                    <a class="lg:px-2" aria-current="page">ShowPiece</a>
+                  </li>
+                </Link>
               </ul>
             </div>
           </div>
