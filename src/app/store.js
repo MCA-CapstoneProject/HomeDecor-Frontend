fb774114b135b217663405b5e91694626589ca13
Sneha Choudhari <snehacc44@gmail.com>
2023-07-04 00:03:14 +0530

    Added redux setup and adjusted ui of homw page

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/authenticate/authSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    authenticate: authReducer,
+  },
 })
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,19 +1,32 @@
-import { Link, Route, Routes } from 'react-router-dom';
-import { ROUTES_ARR } from '../../constants/routes.constants';
-import NotFound from '../../pages/NotFound';
-import SuspenseWrapper from '../../lib/utils/routing/SuspenseWrapper';
-import { useState } from 'react';
+import { Link, Route, Routes } from "react-router-dom";
+import { ROUTES_ARR } from "../../constants/routes.constants";
+import NotFound from "../../pages/NotFound";
+import SuspenseWrapper from "../../lib/utils/routing/SuspenseWrapper";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingBag } from "@fortawesome/fontawesome-free-solid";
 import { faHeart,faUserCircle, } from "@fortawesome/fontawesome-free-regular";
-import ProductsNavbar from './ProductsNavbar';
+// import { useOktaAuth } from '@okta/okta-react';
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import ProductsNavbar from "../ui/ProductsNavbar"
 
 function Navbar() {
-  const [activeLink, setActiveLink] = useState('');
+  const username = useSelector((state) => state.username);
+  // const { authState, authService } = useOktaAuth();
+  
+  // const login = async () => {
+  //   await authService.login('/');
+  // };
+
+  // const logout = async () => {
+  //   await authService.logout('/');
+  // };
+
+  const [activeLink, setActiveLink] = useState("");
 
   const handleLinkClick = (path) => {
     setActiveLink(path);
-  }
+  };
   return (
     <>
       <nav className="w-full ">
@@ -22,21 +35,28 @@ function Navbar() {
             <div className="flex-1 flex items-center justify-between">
               <div className="flex-shrink-0 flex items-center">
                 {/* <span className="text-xl font-bold">Logo</span> */}
-                <img class="object-fill w-[13rem]"
-                  src="assets/logo.png" alt="website-logo" />
+                <img
+                  className="object-fill w-[13rem]"
+                  src="assets/logo.png"
+                  alt="website-logo"
+                />
               </div>
               {/* Pages */}
               <div className="hidden sm:block sm:ml-6">
                 <div className="flex space-x-4">
                   {ROUTES_ARR.map((item) => {
                     return (
-                      <Link key={item.path} to={item.path}
-                        className={`font-thin text-black hover:text-gray-500 hover:font-semibold px-3 py-2 uppercase ${activeLink === item.path ? "text-[#3C4048]" : ""}`}
+                      <Link
+                        key={item.path}
+                        to={item.path}
+                        className={`font-thin text-black hover:text-gray-500 hover:font-semibold px-3 py-2 uppercase ${
+                          activeLink === item.path ? "text-[#3C4048]" : ""
+                        }`}
                         onClick={() => handleLinkClick(item.path)}
                       >
                         {item.name}
                       </Link>
-                    )
+                    );
                   })}
                 </div>
               </div>
@@ -56,16 +76,37 @@ function Navbar() {
                    text-gray-500 transition-colors" icon={faShoppingBag} />
                   {/* Cart */}
                 </Link>
-                <Link key="3" to="/login"
-                  className={` font-thin text-black hover:text-gray-500 hover:font-semibold py-2 uppercase`}
-                // onClick={() => handleLinkClick(item.path)}
-                >
-                  <FontAwesomeIcon className="block text-[30px] font-[200]  pb-0 
-                   text-gray-500 transition-colors" icon={faUserCircle} />
-                  {/* Profile */}
-                  
 
+                <Link
+                  key=""
+                  to=""
+                  className={` font-thin text-black hover:text-gray-500 hover:font-semibold py-2 uppercase`}
+                  // onClick={() => handleLinkClick(item.path)}
+                >
+                  <FontAwesomeIcon
+                    className="block text-[30px] font-[200]  pb-0 
+                   text-gray-500 hover:text-gray-600 transition-colors"
+                    icon={faUserCircle}
+                  />
+                  {/*Profile */}
                 </Link>
+                {!username ? (
+                  <button
+                    key="3"
+                    to="/login"
+                    className="bg-gray-500 text-slate-100 p-2 rounded-md font-bold hover:text-white hover:bg-gray-600"
+                  >
+                    Login
+                  </button>
+                ) : (
+                  <button
+                    key=""
+                    to="/"
+                    className="bg-gray-500 text-slate-100 p-2 rounded-md font-bold hover:text-white hover:bg-gray-600"
+                  >
+                    Logout
+                  </button>
+                )}
               </div>
             </div>
           </div>
@@ -75,14 +116,20 @@ function Navbar() {
       <ProductsNavbar></ProductsNavbar>
       <Routes>
         {ROUTES_ARR.map((item, idx) => (
-          <Route key={idx} path={item.path} element={<SuspenseWrapper><item.component /></SuspenseWrapper>} />
-        ))
-        }
+          <Route
+            key={idx}
+            path={item.path}
+            element={
+              <SuspenseWrapper>
+                <item.component />
+              </SuspenseWrapper>
+            }
+          />
+        ))}
         <Route element={NotFound} />
       </Routes>
     </>
-
-  )
+  );
 }
 
 export default Navbar;
