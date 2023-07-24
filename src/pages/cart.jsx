4dd648856279b05bf1e3c02e452786717256d388
Tausifnsr <mdtausifnsr9@gmail.com>
2023-07-24 14:36:41 +0530

    Cart and Wishlist Pages.

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -2,6 +2,88 @@ import React from 'react'
 
 export default function cart() {
   return (
-    <div>cart</div>
+    <>
+    
+    
+        <div>
+              <div className='text-xl font-bold justify-center text-left ml-40 mt-10 mb-10'>Your Cart(1 Item) </div>
+              <div className='bg-green-100 w-2/5 ml-40 mb-4 p-1 text-center'>
+                <p className='text-green-500 font-semibold'>You Saved $300 On This Order.</p>
+              </div>
+            <div className='flex'>
+              <div className='border-2 w-2/5 h-auto ml-40 mb-10'>
+                <div className='flex'>
+                    <div>
+                        <div className=' h-48 w-48 m-5'>
+                          <img src="../assets/productimages/wall-mirror.jpg" alt="product-image" />
+                        </div>
+                        <div className='ml-5 text-sm'>Enter PIN Code for Delivery <br />
+                                              Details 
+                        </div>
+                        <div>
+                        <button className='bg-teal-400 text-white ml-5 mt-2 p-2 w-48 border-teal-400 text-center hover:text-teal-400 hover:bg-white border-2 '>Move to Wishlist</button> 
+                        </div>
+                        <div> 
+                        <button className='bg-red-400 text-white ml-5 mb-5 mt-2 p-2 text-center w-48 hover:text-red-400 hover:bg-white border-red-400 border-2'>Delete</button>
+                        </div>
+                        
+                    </div>
+                
+                        <div className='mt-5'>
+                                <p>Sculpted Wall Mirror</p>
+                                <p className='text-slate-400'>By MegaArts from Decorica</p>
+                                <p className='border-2 w-24 mt-4 p-2'>QTY <select name="" id="">
+                                  <option value="1"> 1</option>
+                                  <option value="2"> 2</option>
+                                  <option value="3"> 3</option>
+                                  <option value="4"> 4</option>
+                                </select></p>
+                                <div className='flex '>
+                                <div>
+                                <div className='mt-4'>MRP</div>
+                                <div className='text-green-600 mb-2'>Item Discount</div>
+                                <div className='font-semibold border-black border-t-2'>Item Price</div>
+                                </div>
+                                <div className='ml-20'>
+                                <div className='mt-4'>$1299</div>
+                                <div className='text-green-600 mb-2'>$300</div>
+                                <div className='font-semibold border-black border-t-2'>$999</div>
+                                </div>
+                                </div>
+                        </div>
+                  </div> 
+              </div>
+              <div className='ml-5 w-2/5 h-auto float-right'>
+                  <div>
+                    Delivering To <br />
+                    <input className='border-2 w-[500px] h-12 mt-3 pl-4 outline-none' type="text " placeholder='Enter PIN Code' maxLength={6} />
+                  </div>
+                  {/* <div>
+                  <input className='border-2 w-[500px] h-12 mt-3 outline-none pl-4 uppercase' placeholder='Apply Coupon' type="text " maxLength={6} />
+                  </div> */}
+                  <div className='border-2 mt-3 w-[500px] h-auto'>
+                    <h1 className='font-semibold text-center text-xl mt-3'>Cart Summary</h1>
+                    <div className='flex '>
+                                <div className='ml-3'>
+                                <div className='mt-4'>Items in Cart</div>
+                                <div className='mb-2'>Cart Total Price</div>
+                                <div className='font-bold uppercase border-black border-t-2 mt-5'>You Pay</div>
+                                <div className='text-green-600 font-semibold mb-2'>You Saved</div>
+                                </div>
+                                <div className='ml-60'>
+                                <div className='mt-4'>1</div>
+                                <div className='mb-2'>$999</div>
+                                <div className='font-semibold mt-5 border-black border-t-2'>$999</div>
+                                <div className='text-green-600 font-semibold mb-2'>$300</div>
+                                </div>
+                                </div>
+                                
+                  </div>
+                  <button className='bg-red-400 w-[500px] text-white font-bold uppercase mt-3 p-5'>Proceed To Pay</button>
+              </div>
+      </div>
+      
+    </div>
+    </>
   )
 }
--- src/pages/wishlist.jsx
+++ src/pages/wishlist.jsx
@@ -1,7 +1,80 @@
 import React from 'react'
+// import Products from "../constants/wishlist.constants"
 
 export default function Wishlist() {
   return (
-    <div>wishlist</div>
+    <>
+    <div className='m-10 ml-24'>
+      <h1 className='text-xl font-semibold'>My Wishlist(4 Items)</h1>
+    </div>
+        <div className='flex justify-around m-10 mb-36 relative'>
+          <div className='w-[250px] h-[350px]'>
+            <img className='h-[300px] w-[250px]' src="../assets/productimages/gift-set.jpg" alt="" />
+            <div className='mb-2 text-center'>
+              A Special collection of Gift Set
+              <p className='font-semibold text-center mt-2 text-sm'>Rs. 999 <span className='line-through font-thin text-xs ml-2'> Rs. 1299</span> <span className='text-red-500'>(23% Off)</span></p>
+            </div>
+            <div className=' w-[250px]'>
+              <button className='bg-teal-400 text-white mt-2 p-2 w-full border-teal-400 text-center hover:text-teal-400 hover:bg-white border-2'>Add to Cart</button>
+            </div>
+            <div className=' w-[250px]'>
+            <button className='bg-red-400 text-white mb-5 mt-2 p-2 text-center w-full hover:text-red-400 hover:bg-white border-red-400 border-2'>Remove From Wishlist</button>
+            </div>
+          </div>
+          <div className='w-[250px] h-[350px]'>
+          <img className='h-[300px] w-[250px]' src="../assets/productimages/key-holders.jpg" alt="" />
+          <div className='mb-2 text-center'>
+              Handmade Key Holder
+              <p className='font-semibold text-center mt-2 text-sm'>Rs. 532 <span className='line-through font-thin text-xs ml-2'> Rs. 759</span> <span className='text-red-500'>(30% Off)</span></p>
+            </div>
+          <div className=' w-[250px]'>
+              <button className='bg-teal-400 text-white mt-2 p-2 w-full border-teal-400 text-center hover:text-teal-400 hover:bg-white border-2'>Add to Cart</button>
+            </div>
+            <div className=' w-[250px]'>
+            <button className='bg-red-400 text-white mb-5 mt-2 p-2 text-center w-full hover:text-red-400 hover:bg-white border-red-400 border-2'>Remove From Wishlist</button>
+            </div>
+          </div>
+          <div className='w-[250px] h-[350px]'>
+          <img className='h-[300px] w-[250px]' src="../assets/productimages/vase-1.jpg" alt="" />
+          <div className='mb-2 text-center'>
+          Handpainted Matki Vase
+              <p className='font-semibold text-center mt-2 text-sm'>Rs. 499 <span className='line-through font-thin text-xs ml-2'> Rs. 999</span> <span className='text-red-500'>(50% Off)</span></p>
+            </div>
+          <div className=' w-[250px]'>
+              <button className='bg-teal-400 text-white mt-2 p-2 w-full border-teal-400 text-center hover:text-teal-400 hover:bg-white border-2'>Add to Cart</button>
+            </div>
+            <div className=' w-[250px]'>
+            <button className='bg-red-400 text-white mb-5 mt-2 p-2 text-center w-full hover:text-red-400 hover:bg-white border-red-400 border-2'>Remove From Wishlist</button>
+            </div>
+          </div>
+        
+          <div className='w-[250px] h-[350px]'>
+          <img className='h-[300px] w-[250px]' src="../assets/productimages/wall-mask.jpg" alt="" />
+          <div className='mb-2 text-center'>
+              Bamboo Naga Tribal Mask
+              <p className='font-semibold text-center mt-2 text-sm'>Rs. 1499 <span className='line-through font-thin text-xs ml-2'> Rs. 1999</span> <span className='text-red-500'>(25% Off)</span></p>
+            </div>
+          <div className=' w-[250px]'>
+              <button className='bg-teal-400 text-white mt-2 p-2 w-full border-teal-400 text-center hover:text-teal-400 hover:bg-white border-2'>Add to Cart</button>
+            </div>
+            <div className=' w-[250px]'>
+            <button className='bg-red-400 text-white mb-5 mt-2 p-2 text-center w-full hover:text-red-400 hover:bg-white border-red-400 border-2'>Remove From Wishlist</button>
+            </div>
+          </div>
+          
+          {/* <div className='w-[300px] h-[350px] bg-slate-500'>
+            <div className='flex absolute bottom-0 w-[300px]'>
+              <button className='bg-white w-1/2 border-2'>Add to Cart</button>
+              <button className='bg-black text-white w-1/2 h-10'>Buy Now</button>
+            </div>
+          </div>
+          <div className='w-[300px] h-[350px] bg-slate-500'>
+            <div className='flex absolute bottom-0 w-[300px]'>
+              <button className='bg-white w-1/2 border-2'>Add to Cart</button>
+              <button className='bg-black text-white w-1/2 h-10'>Buy Now</button>
+            </div>
+          </div> */}
+          </div>
+    </>
   )
 }
