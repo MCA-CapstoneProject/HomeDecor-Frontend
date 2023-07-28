7edbb7b515fcaf865cfe5e62e8b42b18ddf30b6a
Tausifnsr <mdtausifnsr9@gmail.com>
2023-07-28 14:04:44 +0530

    Error Resolved

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Authentication from "./Authentication.jsx";
 
-<<<<<<< HEAD
-import { useState, useEffect } from "react";
-import ProductCard from "./pages/ProductCard";
-=======
->>>>>>> f70c82853a417602e7aa23b7ed66001c6fbbdeac
 
 const App = () => {
 
