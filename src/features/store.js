0b2e584fd34b1273db8ab90c101b1903bf565014
Sneha Choudhari <snehacc44@gmail.com>
2023-08-17 23:55:41 +0530

    removed unused files

--- src/features/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./authenticate/authSlice";
-
-export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    }
-})
